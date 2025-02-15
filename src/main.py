cbbe55ceb8a034ac399f6b3d4ca21ee44fd17a1a
Patrick Sollars <pjsollars@gmail.com>
2025-02-15 15:33:28 -0600

    Add multiple tag support in get_config_value (with flaws, before I do recursion)

--- src/main.py
+++ src/main.py
@@ -9,24 +9,35 @@ with open("config.yaml") as f:
 NO_DEFAULT = object()
 
 
+def is_primitive(value):
+    return isinstance(value, (int, float, str, bool))
+
+
 def get_config_value(path, default=NO_DEFAULT):
-    config_tags = os.environ.get("CONFIG_TAG", "").split(",")
+    config_tags = os.environ.get("CONFIG_TAG", "").split(",") + [""]
     value = CONFIG
 
     try:
         for key in path.split("."):
+            if not isinstance(value, dict):
+                break
+
             for tag in config_tags:
-                if isinstance(value, dict):
-                    found = value.get(f"{key}[{tag}]")
-                    if found:
-                        value = found
-                        break
-            if isinstance(value, dict):
-                value = value[key]
+                tagged_key = f"{key}[{tag}]" if tag else key
+                if tagged_key in value:
+                    value = value[tagged_key]
+                    if is_primitive(value):
+                        return value
+                    break
+
     except KeyError:
+        pass
+
+    if value is CONFIG or not is_primitive(value):
         if default is NO_DEFAULT:
-            raise
-        value = default
+            raise KeyError(f"No config value found at: {path}")
+        return default
+
     return value
 
 
